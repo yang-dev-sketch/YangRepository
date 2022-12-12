4a6fc15cd88126550f0a1e14eeb76bfbe13a1b5c
zhang0207 <peacedev929@hotmail.com>
2022-12-11 16:32:24 -0800

    chore: insert config files

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    "no-unexpected-multiline": "off",
+    "react-native/no-inline-styles": "off"
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  proseWrap: "preserve",
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  "react/jsx-max-props-per-line": [1, { "when": "always" }]
+};
